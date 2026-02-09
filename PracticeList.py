ec1b868f4d8455463fd6c6524416a27775198f8b
MogGlaze <bmartinez06054@gmail.com>
2026-02-09 14:14:17 -0800

    Create PracticeList.py

---
+++ PracticeList.py
@@ -0,0 +1,8 @@
+x = ['fName', 'lName', 25, True, 4.2 , False, "Petah","Homer,","Birdo"]
+print('x is now a list of mixed objects')
+print(x)
+print(type(x))
+print(x[3])
+print(type(x[3]))
+print(x[2])
+print(type(x[2]))
